da53d31a46f1dd6ede917f97c2778cfb1f5fdd42
Zennyth <matsfox@orange.fr>
2020-07-17 10:58:22 +0200

    added Dragon Boss

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BDDSW---2
 Database of SummonersWar images in order to build an AI that can play the game.
+
+# Giant Test
---
+++ manager.py
@@ -0,0 +1,41 @@
+from fastai2.basics import *
+# import pickle
+
+Wave = { 'L': 'Loading', 'W': 'Wave', 'U': 'Undefined', 'MB': 'MiniBoss', 'B': 'Boss' }
+State = { 'O': 'Ongoing', 'V': 'Victory', 'L': 'Loose' }
+
+filename = 'data.csv'
+fieldnames = ['Name', 'Run', 'Wave', 'State', 'Button']
+example = [{'Name': 'Example1.jpg', 'Run': 'TOA', 'Wave': 'L', 'State': 'O', 'Button': 'False' }, {'Name': 'Example2.jpg', 'Run': 'TOA', 'Wave': 'L', 'State': 'O', 'Button': 'False' }]
+
+imagestemp = get_image_files('temp')
+folder = 'Images'
+
+import csv
+
+# Create
+# with open('data.csv', 'w') as csvfile:
+#     fieldnames = ['Name', 'Run', 'Wave', 'State', 'Button']
+#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+#     writer.writeheader()
+#     writer.writerow({'Name': 'Example2.jpg', 'Run': 'TOA', 'Wave': 'L', 'State': 'O', 'Button': 'False' })
+
+# Write
+def write(filename, fieldnames, data):
+	with open(filename, 'a', newline='') as csvfile:
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		writer.writerows(data)
+
+# Read
+def read(filename):
+	with open(filename, "rt") as csvfile:
+		reader = csv.DictReader(csvfile)
+		for row in reader:
+			print(row.join(', '))
+
+data = []
+for image in imagestemp:
+	data.append({'Name': image.name, 'Run': 'Cairos', 'Wave': 'B', 'State': 'O', 'Button': 'False' })
+
+write(filename, fieldnames, data)
